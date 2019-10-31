ebf17ca44b9a5c578d709bd5ca6bccde3c94d29f
Rucha Deshpande <rucha.d1690@gmail.com>
2019-10-31 16:46:14 +0530

    Updates

---
+++ precache-manifest.6937d48d52739a1b1a3a50b89572bf3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25744a3c94746383c5629d93db021e6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a22a07771fc92367459e",
+    "url": "/robofriends/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "6f9e75a5267b5383547e",
+    "url": "/robofriends/static/css/main.98997687.chunk.css"
+  },
+  {
+    "revision": "a22a07771fc92367459e",
+    "url": "/robofriends/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "6f9e75a5267b5383547e",
+    "url": "/robofriends/static/js/main.a3ca595c.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  }
+]);
