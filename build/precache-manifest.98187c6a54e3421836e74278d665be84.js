5641523fad056dc397fdfa981d44a08b4af15b9a
Rucha Deshpande <rucha.d1690@gmail.com>
2019-10-31 13:04:55 +0530

    react app

---
+++ build/precache-manifest.98187c6a54e3421836e74278d665be84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53915513c818d0b43ed18a9e0de58f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a22a07771fc92367459e",
+    "url": "/static/css/2.299a5230.chunk.css"
+  },
+  {
+    "revision": "fc9b31044ebe23362f2d",
+    "url": "/static/css/main.98997687.chunk.css"
+  },
+  {
+    "revision": "a22a07771fc92367459e",
+    "url": "/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "fc9b31044ebe23362f2d",
+    "url": "/static/js/main.46cebfd3.chunk.js"
+  },
+  {
+    "revision": "c332e1bb67579b93fb1f",
+    "url": "/static/js/runtime-main.b75238ee.js"
+  }
+]);
